fd38d6bcc24aa33f6230daa4a2a7ab28c5ca9830
Michael Bisbjerg <michael@mbwarez.dk>
2013-03-10 22:41:13 +0100

    Fixed issue with IndexAllocations

--- NTFSLib/Objects/Attributes/AttributeIndexAllocation.cs
+++ NTFSLib/Objects/Attributes/AttributeIndexAllocation.cs
@@ -1,25 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
+﻿using System.Collections.Generic;
 using NTFSLib.Objects.Enums;
+using NTFSLib.Objects.Specials;
 
 namespace NTFSLib.Objects.Attributes
 {
     public class AttributeIndexAllocation : Attribute
     {
-        public string Signature { get; set; }
-        public ushort OffsetToUSN { get; set; }
-        public ushort USNSizeWords { get; set; }
-        public ulong LogFileUSN { get; set; }
-        public ulong VCNInIndexAllocation { get; set; }
-        public uint OffsetToFirstIndex { get; set; }
-        public uint SizeOfIndexTotal { get; set; }
-        public uint SizeOfIndexAllocated { get; set; }
-        public byte HasChildren { get; set; }
-        public byte[] USNNumber { get; set; }
-        public byte[] USNData { get; set; }
-
+        public IndexAllocationChunk[] Indexes { get; set; }
         public IndexEntry[] Entries { get; set; }
 
         public override AttributeResidentAllow AllowedResidentStates
@@ -32,76 +19,25 @@ namespace NTFSLib.Objects.Attributes
 
         internal override void ParseAttributeNonResidentBody(NTFS ntfs)
         {
-            base.ParseAttributeNonResidentBody(ntfs);
-
-            // Get all chunks
             byte[] data = Utils.ReadFragments(ntfs, NonResidentHeader.Fragments);
 
-            Debug.Assert(data.Length >= 36);
-
-            // Parse
-            Signature = Encoding.ASCII.GetString(data, 0, 4);
-
-            Debug.Assert(Signature == "INDX");
-
-            OffsetToUSN = BitConverter.ToUInt16(data, 4);
-            USNSizeWords = BitConverter.ToUInt16(data, 6);
-            LogFileUSN = BitConverter.ToUInt64(data, 8);
-            VCNInIndexAllocation = BitConverter.ToUInt64(data, 16);
-            OffsetToFirstIndex = BitConverter.ToUInt32(data, 24);
-            SizeOfIndexTotal = BitConverter.ToUInt32(data, 28);
-            SizeOfIndexAllocated = BitConverter.ToUInt32(data, 32);
-            HasChildren = data[36];
-
-            Debug.Assert(data.Length >= OffsetToUSN + 2 + USNSizeWords * 2);
-
-            USNNumber = new byte[2];
-            Array.Copy(data, OffsetToUSN, USNNumber, 0, 2);
-
-            USNData = new byte[USNSizeWords * 2];
-            Array.Copy(data, OffsetToUSN + 2, USNData, 0, USNSizeWords * 2);
-
-            // Patch USN Data
-            ApplyUSNPatch(data, ((int)SizeOfIndexAllocated + 24) / ntfs.Boot.BytesPrSector, ntfs.Boot.BytesPrSector);
-
-            Debug.Assert(SizeOfIndexTotal <= data.Length);
-
-            // Parse entries
+            List<IndexAllocationChunk> indexes = new List<IndexAllocationChunk>();
             List<IndexEntry> entries = new List<IndexEntry>();
 
-            int pointer = (int)(OffsetToFirstIndex + 24);       // Offset is relative to 0x18
-            while (pointer <= SizeOfIndexTotal + 24)
+            // Parse
+            for (int i = 0; i < NonResidentHeader.Fragments.Length; i++)
             {
-                IndexEntry entry = IndexEntry.ParseData(data, (int)SizeOfIndexTotal - pointer + 24, pointer);
+                for (int j = 0; j < NonResidentHeader.Fragments[i].Clusters; j++)
+                {
+                    IndexAllocationChunk index = IndexAllocationChunk.ParseBody(ntfs, data, (int)(NonResidentHeader.Fragments[i].StartingVCN * ntfs.BytesPrCluster + j * ntfs.BytesPrCluster));
 
-                if (entry.Flags.HasFlag(MFTIndexEntryFlags.LastEntry))
-                    break;
-
-                entries.Add(entry);
-
-                pointer += entry.Size;
+                    indexes.Add(index);
+                    entries.AddRange(index.Entries);
+                }
             }
 
+            Indexes = indexes.ToArray();
             Entries = entries.ToArray();
-        }
-
-        private void ApplyUSNPatch(byte[] data, int sectors, ushort bytesPrSector)
-        {
-            Debug.Assert(data.Length >= sectors * bytesPrSector);
-
-            for (int i = 0; i < sectors; i++)
-            {
-                // Get pointer to the last two bytes
-                int blockOffset = i * bytesPrSector + 510;
-
-                // Check that they match the USN Number
-                Debug.Assert(data[blockOffset] == USNNumber[0]);
-                Debug.Assert(data[blockOffset + 1] == USNNumber[1]);
-
-                // Patch in new data
-                data[blockOffset] = USNData[i * 2];
-                data[blockOffset + 1] = USNData[i * 2 + 1];
-            }
         }
     }
 }
---
+++ NTFSLib/Objects/Specials/IndexAllocationChunk.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using NTFSLib.Objects.Enums;
+
+namespace NTFSLib.Objects.Specials
+{
+    public class IndexAllocationChunk
+    {
+        public string Signature { get; set; }
+        public ushort OffsetToUSN { get; set; }
+        public ushort USNSizeWords { get; set; }
+        public ulong LogFileUSN { get; set; }
+        public ulong VCNInIndexAllocation { get; set; }
+        public uint OffsetToFirstIndex { get; set; }
+        public uint SizeOfIndexTotal { get; set; }
+        public uint SizeOfIndexAllocated { get; set; }
+        public byte HasChildren { get; set; }
+        public byte[] USNNumber { get; set; }
+        public byte[] USNData { get; set; }
+
+        public IndexEntry[] Entries { get; set; }
+
+        public static IndexAllocationChunk ParseBody(NTFS ntfs, byte[] data, int offset)
+        {
+            Debug.Assert(data.Length >= 36);
+
+            IndexAllocationChunk res = new IndexAllocationChunk();
+
+            // Parse
+            res.Signature = Encoding.ASCII.GetString(data, offset + 0, 4);
+
+            Debug.Assert(res.Signature == "INDX");
+
+            res.OffsetToUSN = BitConverter.ToUInt16(data, offset + 4);
+            res.USNSizeWords = BitConverter.ToUInt16(data, offset + 6);
+            res.LogFileUSN = BitConverter.ToUInt64(data, offset + 8);
+            res.VCNInIndexAllocation = BitConverter.ToUInt64(data, offset + 16);
+            res.OffsetToFirstIndex = BitConverter.ToUInt32(data, offset + 24);
+            res.SizeOfIndexTotal = BitConverter.ToUInt32(data, offset + 28);
+            res.SizeOfIndexAllocated = BitConverter.ToUInt32(data, offset + 32);
+            res.HasChildren = data[36];
+
+            Debug.Assert(data.Length >= offset + res.OffsetToUSN + 2 + res.USNSizeWords * 2);
+
+            res.USNNumber = new byte[2];
+            Array.Copy(data, offset + res.OffsetToUSN, res.USNNumber, 0, 2);
+
+            res.USNData = new byte[res.USNSizeWords * 2];
+            Array.Copy(data, offset + res.OffsetToUSN + 2, res.USNData, 0, res.USNSizeWords * 2);
+
+            // Patch USN Data
+            ApplyUSNPatch(data, offset, ((int)res.SizeOfIndexAllocated + 24) / ntfs.Boot.BytesPrSector, ntfs.Boot.BytesPrSector, res.USNNumber, res.USNData);
+
+            Debug.Assert(offset + res.SizeOfIndexTotal <= data.Length);
+
+            // Parse entries
+            List<IndexEntry> entries = new List<IndexEntry>();
+
+            int pointer = offset + (int)(res.OffsetToFirstIndex + 24);       // Offset is relative to 0x18
+            while (pointer <= offset + res.SizeOfIndexTotal + 24)
+            {
+                IndexEntry entry = IndexEntry.ParseData(data, offset + (int)res.SizeOfIndexTotal - pointer + 24, pointer);
+
+                if (entry.Flags.HasFlag(MFTIndexEntryFlags.LastEntry))
+                    break;
+
+                entries.Add(entry);
+
+                pointer += entry.Size;
+            }
+
+            res.Entries = entries.ToArray();
+
+            return res;
+        }
+
+        private static void ApplyUSNPatch(byte[] data, int offset, int sectors, ushort bytesPrSector, byte[] USNNumber, byte[] USNData)
+        {
+            Debug.Assert(data.Length >= offset + sectors * bytesPrSector);
+
+            for (int i = 0; i < sectors; i++)
+            {
+                // Get pointer to the last two bytes
+                int blockOffset = offset + i * bytesPrSector + 510;
+
+                // Check that they match the USN Number
+                Debug.Assert(data[blockOffset] == USNNumber[0]);
+                Debug.Assert(data[blockOffset + 1] == USNNumber[1]);
+
+                // Patch in new data
+                data[blockOffset] = USNData[i * 2];
+                data[blockOffset + 1] = USNData[i * 2 + 1];
+            }
+        }
+    }
+}
